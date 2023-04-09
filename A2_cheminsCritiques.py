58d9197c06c0ca9f9d7c7fb55f2605709587aa13
Jarilfrey <73791849+Jarilfrey@users.noreply.github.com>
2023-04-09 13:49:56 +0200

    Update A2_cheminsCritiques.py
    Ajout de commentaire supplémentaire

--- A2_cheminsCritiques.py
+++ A2_cheminsCritiques.py
@@ -17,6 +17,7 @@ def cheminsCritique(matrice):
     # On trouve les tâches qui sont sur le(s) chemin(s) critique(s)
     chemin_critique = []
     for i in range(nb_taches):
+        # Si la marge de l'activité est nulle, elle appartient au chemin critique
         if marges[i] == 0:
             chemin_critique.append(i)
 
@@ -26,9 +27,13 @@ def cheminsCritique(matrice):
         chemin = [tache]
         temp = [tache]
         while temp:
+            # On récupère la première tâche précédente de la liste temporaire
             tache_precedente = temp.pop(0)
             for i in range(nb_taches):
+                # Si la tâche actuelle est précédente de la tâche précédente, elle appartient aussi au chemin critique
                 if matrice[i][tache_precedente] == 1:
+                    # On insère la tâche actuelle au début de la liste du chemin
                     chemin.insert(0, i)
+                    # On ajoute la tâche actuelle à la liste temporaire pour chercher ses tâches précédentes
                     temp.append(i)
         print(chemin)
